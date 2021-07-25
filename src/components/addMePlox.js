f34fbad2844a9cc60dd62682b875b2ae759a9016
Nick Papadakis <nickfin2014@gmail.com>
2021-07-25 15:16:57 -0700

    rebase complete

--- src/components/addMePlox.js
+++ src/components/addMePlox.js
@@ -2,38 +2,4 @@
 //what you know about that
 //asjkasjiaijsaijjsi
 //SSH configuration good to go
-<<<<<<< HEAD
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-//what you know about that
-//asjkasjiaijsaijjsi
-//SSH configuration good to go
-
-=======
 //git commit -am flag test
->>>>>>> -am flag test

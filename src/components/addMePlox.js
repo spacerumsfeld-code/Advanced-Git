41a54a5dccac3cca1045b41ee8d21be662384fee
Nick Papadakis <nickfin2014@gmail.com>
2021-07-25 15:00:20 -0700

    -am flag test

--- src/components/addMePlox.js
+++ src/components/addMePlox.js
@@ -2,6 +2,7 @@
 //what you know about that
 //asjkasjiaijsaijjsi
 //SSH configuration good to go
+<<<<<<< HEAD
 //what you know about that
 //asjkasjiaijsaijjsi
 //SSH configuration good to go
@@ -33,3 +34,6 @@
 //asjkasjiaijsaijjsi
 //SSH configuration good to go
 
+=======
+//git commit -am flag test
+>>>>>>> -am flag test
